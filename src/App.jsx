269a81f13ae17e61054a414700990da79602cba8
codingwizard88 <mahmoud@mightydigits.com>
2022-04-29 21:39:55 +0300

    precyprus

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
 
   return (
     <div className="App">
-      <div className="projects h-[80vh] w-10/12 overflow-auto relative z-10 bg-white rounded-xl ring-1 ring-slate-900/5 divide-y divide-slate-100 my-auto xl:mt-18 dark:bg-slate-800 dark:divide-slate-200/5 dark:highlight-white/10">
-        <div className="list divide-y divide-slate-100 grid grid-cols-2">
+      <div className="projects h-[80vh] w-10/12 relative z-10 bg-white rounded-xl ring-1 ring-slate-900/5 divide-y divide-slate-100 my-auto xl:mt-18 dark:bg-slate-800 dark:divide-slate-200/5 dark:highlight-white/10">
+        <div className="list divide-y divide-slate-100 gap-4 grid grid-cols-1 md:grid-cols-2">
           {projects.map((project) => (
             <ListItem item={project} />
           ))}
